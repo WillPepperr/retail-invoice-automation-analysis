9273648f49d66318fb43232d8586cb9ed517668c
WillPepperr <will.pepp3r@gmail.com>
2025-12-27 16:29:28 -0600

    fixed uml and added schema diagrams

--- rewrite_identity.py
+++
@@ -1,9 +0,0 @@
-﻿import os
-
-name  = os.environ.get('NEW_NAME', '').encode('utf-8')
-email = os.environ.get('NEW_EMAIL', '').encode('utf-8')
-
-commit.author_name = name
-commit.author_email = email
-commit.committer_name = name
-commit.committer_email = email
